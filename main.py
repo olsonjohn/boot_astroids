082a5b2f563fef958ff7265ab5d4e8016c951522
olsonjohn <n0atn86@gmail.com>
2024-09-14 20:33:16 -0500

    added stuff

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock_object = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -15,6 +17,8 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+        time_called = clock_object.tick(60)
+        dt = time_called / 1000
 
 
 if __name__ == "__main__":
